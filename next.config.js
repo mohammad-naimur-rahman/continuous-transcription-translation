5054e63cfa0a27a2249e937962df3fff66f175f6
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2025-01-29 18:01:08 +0600

    update: next config, opt out output true

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-};
+    ignoreDuringBuilds: true
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
